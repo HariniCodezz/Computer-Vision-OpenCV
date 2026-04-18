40c8e8395de852b99260dce8107ab3a94a40b25d
HariniCodezz <mail2hariniv@gmail.com>
2026-04-18 21:04:20 +0530

    GrayScale Code

---
+++ 01webCam/grayScale.py
@@ -0,0 +1,12 @@
+import cv2
+capture=cv2.VideoCapture(0)
+while True:
+    ret,frame=capture.read()
+    if not ret:
+        break
+    gray=cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
+    cv2.imshow("Gray image", gray)
+    if cv2.waitKey(1)==ord('q'):
+        break
+capture.release()
+cv2.destroyAllWindows()
